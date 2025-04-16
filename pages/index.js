c8ff21a274a1a4d9cabc53ab0bc8b3f2fbab0616
Adam Harris <adamtjharris@gmail.com>
2025-04-16 07:38:48 +0200

    add basic routing for Overview, Details and Favourites pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <footer>
+        <ul>
+          <li>Spotlist</li>
+          <li>Art Pieces</li>
+          <li>Favourites</li>
+        </ul>
+      </footer>
     </div>
   );
 }
