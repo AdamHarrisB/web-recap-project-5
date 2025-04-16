8e2d5c75c32c076cc0e9c1eeb64025db3c8aac9b
Adam Harris <adamtjharris@gmail.com>
2025-04-16 11:29:17 +0200

    add math.random() works, but unsure why

--- components/SpotlightDisplay/SpotlightDisplay.js
+++ components/SpotlightDisplay/SpotlightDisplay.js
@@ -1,16 +1,30 @@
-import {
-    Card,
-    Image,
-    Name,
-    Artist
-} from "/Styles/styles.js";
+import { ArtWorkDisplay } from "../ArtWorkDisplay/ArtWorkDisplay";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function SpotlightDisplay(){
+    const [SpotlightWork, setsSpotlightWork] = 
+    useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+     useEffect(() => {
+            async function fetchData() {
+                try {
+                    const response = await fetch("https://example-apis.vercel.app/api/art");
+                    const data = await response.json();
+                    const randomIndex = Math.floor(Math.random() * data.length);
+                    setsSpotlightWork(data[randomIndex]);
+                    setIsLoading(false);
+                  } catch (err) {
+                    setError(err);
+                    setIsLoading(false);
+                  }
+                }
+                fetchData();
+              }, []);
+    
     return (
-        <Card>
-        <Image src={work.imageSource} alt="Not Found" />
-        <Name>{work.name}</Name>
-        <Artist>{work.artist}</Artist>
-    </Card>
+        <ArtWorkDisplay work={SpotlightWork} />
 );  
 }
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ use link from Next.js
 */
 
 import Link from "next/link"
+import { SpotlightDisplay } from "@/components/SpotlightDisplay/SpotlightDisplay";
 
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <SpotlightDisplay />
       <footer>
         <Link href="art-pieces">Art Pieces</Link>
         <Link href="favourites">Favourites</Link>
